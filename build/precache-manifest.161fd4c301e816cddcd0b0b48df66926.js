417d733da55e3ed35be42faa044e613039e70552
Stephen <sndegwa.n@outlook.com>
2020-06-03 10:42:57 +0300

    add build folder

---
+++ build/precache-manifest.161fd4c301e816cddcd0b0b48df66926.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba1034a526316fc3f997eda4d3a60511",
+    "url": "/index.html"
+  },
+  {
+    "revision": "065f9eca7be3a84c4507",
+    "url": "/static/css/main.75d26f24.chunk.css"
+  },
+  {
+    "revision": "8ef4322e18fdaa078918",
+    "url": "/static/js/2.7f5e1796.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.7f5e1796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "065f9eca7be3a84c4507",
+    "url": "/static/js/main.70dd5bfc.chunk.js"
+  },
+  {
+    "revision": "a41d4850f0b45938caef",
+    "url": "/static/js/runtime-main.7708cfe5.js"
+  }
+]);
